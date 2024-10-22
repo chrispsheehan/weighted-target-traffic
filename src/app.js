b9a437b335742a8599075e0c78c627c3ce89f552
chrispsheehan <chrispsheehan@gmail.com>
2024-10-22 09:55:23 +0100

    docker file fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const app = require('./lambda');
 const port = process.env.PORT;
 
+console.log(`App starting with PORT: ${port}`);
+
 app.listen(port, () => {
   console.log(`app listening on http://localhost:${port}`);
 });

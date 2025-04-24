f72487795b97dc76c9078b0bffb105a2f09ce05d
chrispsheehan <chrispsheehan@gmail.com>
2025-04-24 12:01:04 +0100

    fix: separate sec group rules

--- tf/security/main.tf
+++ tf/security/main.tf
@@ -1,81 +1,98 @@
 resource "aws_security_group" "ecs_sg" {
   vpc_id = data.aws_vpc.private.id
   name   = local.ecs_security_group_name
+}
 
-  ingress {
-    description     = "Allow ingress from the load balancer to the ecs container port"
-    from_port       = var.ecs_container_port
-    to_port         = var.ecs_container_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
-  }
+resource "aws_security_group_rule" "ecs_ingress_from_lb" {
+  type                     = "ingress"
+  from_port                = var.ecs_container_port
+  to_port                  = var.ecs_container_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
+  description              = "Allow ingress from the load balancer to the ECS container port"
+}
 
-  egress {
-    description = "Allow ECS to talk to VPC endpoints and internal services"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.private.cidr_block]
-  }
+resource "aws_security_group_rule" "ecs_egress_to_vpc" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_blocks       = [data.aws_vpc.private.cidr_block]
+  description       = "Allow ECS to talk to VPC endpoints and internal services"
 }
 
 resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.private.id
   name   = local.lambda_security_group_name
-
-  ingress {
-    description     = "Allow ingress from the load balancer to the lambda function on lambda port 65535"
-    from_port       = 0
-    to_port         = 65535 
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
-  }
-
-  egress {
-    description = "Allow Lambda to send logs to CloudWatch Logs (HTTPS)"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
+resource "aws_security_group_rule" "lambda_ingress_from_lb" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.lambda_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
+  description              = "Allow ingress from the load balancer to the Lambda function on port range 0-65535"
+}
+
+resource "aws_security_group_rule" "lambda_egress_to_logs" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.lambda_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow Lambda to send logs to CloudWatch Logs (HTTPS)"
+}
 resource "aws_security_group" "lb_sg" {
   vpc_id = data.aws_vpc.private.id
   name   = local.lb_security_group_name
-
-  ingress {
-    from_port   = var.load_balancer_port
-    to_port     = var.load_balancer_port
-    protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
-  }
-
-  egress {
-    description = "limit outgoing traffic to the VPC link"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.private.cidr_block]
-  }
 }
 
+resource "aws_security_group_rule" "lb_ingress_from_private_subnets" {
+  type              = "ingress"
+  from_port         = var.load_balancer_port
+  to_port           = var.load_balancer_port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.lb_sg.id
+  cidr_blocks       = local.private_subnet_cidrs
+  description       = "Allow ingress from private subnets to the ALB listener"
+}
+
+resource "aws_security_group_rule" "lb_egress_to_vpc" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.lb_sg.id
+  cidr_blocks       = [data.aws_vpc.private.cidr_block]
+  description       = "Limit outgoing traffic from ALB to VPC"
+}
 resource "aws_security_group" "api_gateway_vpc_link" {
   name        = local.vpc_link_security_group_name
   description = "Security group for API Gateway VPC link"
   vpc_id      = data.aws_vpc.private.id
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
-  }
+resource "aws_security_group_rule" "vpc_link_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.api_gateway_vpc_link.id
+  cidr_blocks       = local.private_subnet_cidrs
+  description       = "Allow HTTP ingress from private subnets to API Gateway VPC Link"
+}
 
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
-  }
+resource "aws_security_group_rule" "vpc_link_egress_http" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.api_gateway_vpc_link.id
+  cidr_blocks       = local.private_subnet_cidrs
+  description       = "Allow HTTP egress from API Gateway VPC Link to private subnets"
 }

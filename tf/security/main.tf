f3dbe9cac3b82950bb7a26e8f5718a4915ca8fa9
chrispsheehan <chrispsheehan@gmail.com>
2025-04-24 11:35:25 +0100

    chore: lock down lambda sec group

--- tf/security/main.tf
+++ tf/security/main.tf
@@ -24,20 +24,19 @@ resource "aws_security_group" "lambda_sg" {
   name   = local.lambda_security_group_name
 
   ingress {
-    description     = "Allow ingress from the load balancer to the lambda function"
+    description     = "Allow ingress from the load balancer to the lambda function on lambda port 65535"
     from_port       = 0
-    to_port         = 0
-    protocol        = -1
+    to_port         = 65535 
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb_sg.id]
   }
 
   egress {
-    description      = "allow lambda to access the internet"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    description = "Allow Lambda to send logs to CloudWatch Logs (HTTPS)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 

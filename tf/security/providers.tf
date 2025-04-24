803eeed86a4f42490191b220395bdd9ef0995b79
chrispsheehan <chrispsheehan@gmail.com>
2025-04-24 10:21:41 +0100

    fix: use correct state s3

--- tf/security/providers.tf
+++ tf/security/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" {
     bucket         = "chrispsheehan-weighted-target-traffic-tfstate"
-    key            = "repo-state/terraform.tfstate"
+    key            = "security-state/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "chrispsheehan-weighted-target-traffic-tf-lockid"

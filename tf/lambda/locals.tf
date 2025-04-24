920245abf5ce35617129438212b59e1e0b5bae5d
chrispsheehan <chrispsheehan@gmail.com>
2025-04-24 11:17:36 +0100

    fix: fmt

--- tf/lambda/locals.tf
+++ tf/lambda/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  lambda_runtime = "nodejs18.x"
-  lambda_handler = "lambda.handler"
-  lambda_name    = var.project_name
-  lambda_bucket  = "${local.lambda_name}-bucket"
-  lambda_security_group_name   = "${var.project_name}-lambda-sg"
+  lambda_runtime             = "nodejs18.x"
+  lambda_handler             = "lambda.handler"
+  lambda_name                = var.project_name
+  lambda_bucket              = "${local.lambda_name}-bucket"
+  lambda_security_group_name = "${var.project_name}-lambda-sg"
 }
--- tf/network/locals.tf
+++ tf/network/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  lb_name          = "${var.project_name}-lb"
-  healthcheck_path = "/health"
+  lb_name                      = "${var.project_name}-lb"
+  healthcheck_path             = "/health"
   lb_security_group_name       = "${var.project_name}-lb-sg"
   vpc_link_security_group_name = "${var.project_name}-api-gateway-vpc-link-sg"
 }
